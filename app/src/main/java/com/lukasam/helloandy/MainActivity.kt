448451270cd665e787da26322011143f6874529a
Lukas A. Mueller <hello@lukasam.com>
2017-07-23 16:19:42 +0200

    Take first steps (Work with buttons and listeners)

--- app/src/main/java/com/lukasam/helloandy/MainActivity.kt
+++ app/src/main/java/com/lukasam/helloandy/MainActivity.kt
@@ -2,11 +2,25 @@ package com.lukasam.helloandy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        increase.setOnClickListener {
+            currentValue.text = (currentValue.text.toString().toInt() + 1).toString()
+        }
+
+        decrease.setOnClickListener {
+            var curVal  = currentValue.text.toString().toInt()
+            when (curVal) {
+                0 -> print("Doesn't work.")
+                else -> currentValue.text = (curVal - 1).toString()
+            }
+        }
+
     }
 }
